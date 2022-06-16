0351a66c1bf881f278692134b45f5e77ae13077d
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-06-16 19:47:41 +0100

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,13 @@
+// Complete the solution so that it reverses the string passed into it.
+//Example:
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+
+
+//Solution:
+// function solution(str){
+//     let splitString = str.split("");
+//     let reverseArray = splitString.reverse();
+//     let joinArray = reverseArray.join("");
+//     return joinArray;
+//  }
