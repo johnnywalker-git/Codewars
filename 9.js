4249de4e0c8172f69588db4e27242c3f7d980b79
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-07-06 23:57:12 +0100

    Create 9.js

---
+++ 9.js
@@ -0,0 +1,20 @@
+// // Problem:
+
+// // Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+
+// Solution:
+
+// var min = function(list){
+//     list.sort(function(a,b) {
+//       return a - b
+//     })
+//         return list[0];
+//     }
+    
+//     var max = function(list){
+//     list.sort(function(a,b) {
+//       return b - a
+//     })
+//         return list[0];
+//     }
