98ca0bb313497823989d4b0e7c3846db9edddc7c
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-07-07 16:30:51 +0100

    Create 10.js

---
+++ 10.js
@@ -0,0 +1,16 @@
+// Problem
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+
+// Solution
+
+// function stray(numbers) {
+//     return numbers.find((e) => numbers.filter((v) => v===e).length === 1); 
+    
+//    } 
+    
