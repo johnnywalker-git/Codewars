b639e1c630dc3a4feb560234cdf986950c774e92
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-07-11 23:46:25 +0100

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,12 @@
+// Problem:
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+
+// Solution:
+// function find_average(array) {
+//     const average = array.reduce((a, b) => a + b, 0) / array.length;
+//       return average;
+    
+//     }
