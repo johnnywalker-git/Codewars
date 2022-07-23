a13f57655ab4a5149bd7e6e8e9f2b5f5cf7a0575
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-07-23 10:41:57 +0100

    Create 19.js

---
+++ 19.js
@@ -0,0 +1,11 @@
+// Problem:
+// You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+// Solution:
+
+// function doubleInteger(i) {
+//     return i * 2
+//     }
