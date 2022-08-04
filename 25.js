236bc7530308e421a9e9d58bafdb18e0ccdf6e1b
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-08-04 18:21:25 +0100

    Create 25.js

---
+++ 25.js
@@ -0,0 +1,11 @@
+// Problem:
+
+// Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+// Solution:
+
+// function roundToNext5(n){
+
+//     return Math.ceil(n / 5) * 5
+    
+//     }
