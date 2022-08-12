d85526686099817f7e921e88adb6bb29e83331a4
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-08-12 20:43:02 +0300

    Create 31.js

---
+++ 31.js
@@ -0,0 +1,12 @@
+// Problem:
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// Solution:
+
+// function sumStr(a,b) {
+//     let result = Number(a) + Number(b);
+//     return result.toString()
+//   }
