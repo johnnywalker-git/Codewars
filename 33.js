fd8473f8317c75a0b0596cece466f76d3cc6a18d
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-01-03 23:09:39 +0000

    Create 33.js

---
+++ 33.js
@@ -0,0 +1,11 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+
+
+
+
+
+// const highAndLow = nums => {
+//     nums = nums.split(" ");
+//     return `${Math.max(...nums)} ${Math.min(...nums)}`;
+//   }
