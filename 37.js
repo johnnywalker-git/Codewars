a1eddbc4303c54905180f38082d5e985e3bd14e5
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-01-09 23:02:14 +0000

    Create 37.js

---
+++ 37.js
@@ -0,0 +1,8 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Solution:
+// function removeEveryOther(arr){
+//     let i = arr.length;
+//     while (i--) (i + 1) % 2 === 0 && (arr.splice(i, 1));
+//       return arr
+//     }
